87ec7c81b30be7115bcbf0d1d513262ab39569ec
PC <TheRainbowPhoenix@users.noreply.github.com>
2025-02-16 15:08:29 +0100

    Add working unpacker in python

---
+++ tools/README.md
@@ -0,0 +1,6 @@
+# HHK3 Patcher
+
+```bash
+python3 path/to/tools/hollyhock.py extract path/to/v02.01.2000.0000/OSupdateDLL.dll path/to/v02.01.2000.0000/fw3069 path/to/v02.01.2000.0000/fw3070
+
+```
---
+++ tools/constants.py
@@ -0,0 +1,58 @@
+# Checksums are SHA256
+# Side note for future self or maintainers:
+#    'missing_byte_X_offset' are the missing bit found around loc_10003A3B and loc_10003A72 
+#    They are basically that byte value, offset from the DLL content.
+#    0x2E88 means the 11912 th bit from the OSUpdateDLL, that is the same as the one found on the updater.
+#    Dunno why it's done that way and not hardcoding missing bits, maybe it's a DRM ?
+#    Won't change for now, as hash guarantee we're all using the same file...
+VERSION_SPECIFIC_INFO = {
+	'v02.01.2000.0000': {
+		'OSupdateDLL_checksum': '22DC7E129F8840E8BAB9490EE96D05C825C98E6060DD27FC66B523CE67180EDA',
+		'missing_byte_3069_offset': 0x2E88,
+		'missing_byte_3070_offset': 0x2E51
+	},
+	'v02.01.4000.0000': {
+		'OSupdateDLL_checksum': '06C8E87CA6E4A8463212141A657D01BD373D78ED84EBCF555891C27B391D60E3',
+		'missing_byte_3069_offset': 0x2E88,
+		'missing_byte_3070_offset': 0x2E51
+	},
+	'v02.01.5000.0000': {
+		'OSupdateDLL_checksum': '1B9296674C65645AF403DD5D21A2EB8FD231B2DC6E716BB23E3F39F77A916BF7',
+		'missing_byte_3069_offset': 0x2E88,
+		'missing_byte_3070_offset': 0x2E51
+	},
+	'v02.01.6000.0000': {
+		'OSupdateDLL_checksum': 'E26F2366AC485931F51592DCE4119570F2207ECCC3762402624FB5D0DBE54409',
+		'missing_byte_3069_offset': 0x2E88,
+		'missing_byte_3070_offset': 0x2E51
+	},
+	'v02.01.7001.0000': {
+		'OSupdateDLL_checksum': '52CC500A65CD495AB9C0FC642C06510670675A57EAA2EAB15E8A744BBE46C67F',
+		'missing_byte_3069_offset': 0x2E88,
+		'missing_byte_3070_offset': 0x2E51
+	},
+	'v02.01.7002.0001': {
+		'OSupdateDLL_checksum': '93BA8B21BB39F1151D6C12A8D0353AC4DD211EE6E60460D1C734A7CFA54F4B37',
+		'missing_byte_3069_offset': 0x2E57, # ABS changed
+		'missing_byte_3070_offset': 0x2E55
+	}
+}
+
+COMPRESSED_IMAGE_HEADER = [0x1F, 0x8B, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00]
+MISSING_BYTE_OFFSET = 0x2FF6
+
+GZIP_COMPRESSION_LEVEL = 6
+
+def find_version_from_OSupdateDLL_checksum(checksum):
+	"""Determines the firmware version with the checksum of the OSupdateDLL.dll file.
+
+	Args:
+		checksum: The SHA256 checksum of the OSupdateDLL.dll file.
+
+	Returns:
+		The version, or None if the checksum did not match any known version.
+	"""
+
+	for version, data in VERSION_SPECIFIC_INFO.items():
+		if data['OSupdateDLL_checksum'].lower() == checksum.lower():
+			return version
